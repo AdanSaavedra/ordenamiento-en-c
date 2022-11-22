1a0f2de304cf083ebf3fbff96afb1a5ac558234c
Adan Saavedra <adansaavedraa@icloud.com>
2022-11-21 20:22:45 -0500

    Initial Commit

---
+++ Lab5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab5
+//
+//  Created by Adan Saavedra on 11/21/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
